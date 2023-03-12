2c0712c98ebff02309178a0264807be53825ecfc
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-12 12:23:47 +0100

    animation and code added

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const qrText = document.getElementById('qrText')
 const btn = document.querySelector('button')
 
 function generateQR() {
-    qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
+    if(qrText.value.length > 0 ) {
+        qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
     imgBox.classList.add('show-img')
+    } else {
+        qrText.classList.add('error')
+        setTimeout(() => {
+            qrText.classList.remove('error')
+        },1000)
+    }
+    
 }
 
 btn.addEventListener('click', generateQR)
