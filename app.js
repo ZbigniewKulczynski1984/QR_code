b433ad0be78c6e54f590deb256a4d49350d2e4f5
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-12 12:11:36 +0100

    style and code added

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const imgBox = document.getElementById('imgBox')
 const qrImage = document.getElementById('qrImage')
 const qrText = document.getElementById('qrText')
+const btn = document.querySelector('button')
 
-function GenerateQR() {
-    qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value
+function generateQR() {
+    qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
+    imgBox.classList.add('show-img')
 }
+
+btn.addEventListener('click', generateQR)
+
