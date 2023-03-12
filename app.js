9ff62de09ecc23b14377fc4bde5b33c4146f4708
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-12 12:42:00 +0100

    code added

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const qrImage = document.getElementById('qrImage')
 const qrText = document.getElementById('qrText')
 const btn = document.querySelector('button')
 
+function showQR() {qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+imgBox.classList.add('show-img')} 
+
+
 function generateQR() {
     if(qrText.value.length > 0 ) {
-        qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
-    imgBox.classList.add('show-img')
+        showQR()
     } else {
         qrText.classList.add('error')
         setTimeout(() => {
@@ -17,4 +20,11 @@ function generateQR() {
 }
 
 btn.addEventListener('click', generateQR)
+
+qrText.addEventListener('keypress', function(event)  {
+    if(event.key === 'Enter') {
+        event.preventDefault()
+        showQR()
+    }
+})
 
